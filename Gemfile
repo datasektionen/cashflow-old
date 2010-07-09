d75896217d3553ffdfead7d9953cb4e694e3929d
Martin Frost <frost@ceri.se>
2010-07-09 20:07:54 +0200

    lagt till grundläggande skelett, inklusive haml/sass/compass/blueprint

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta4'
+gem 'capistrano'
+gem 'haml'
+gem 'compass', '>0.10.1'
+
+group :production do
+  gem 'mysql'
+end
+
+group :development do
+  gem 'sqlite3-ruby', :require => 'sqlite3'
+  gem 'ruby-debug'
+end
+
+# Gems for test environment
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'cucumber'
+  gem 'rspec-rails'
+  gem 'spork'
+  gem 'launchy'    # So you can do Then show me the page
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '09a55b6ac022b75d133d9111e389542ba7f36b18c71e1eea303e7cf1dc81fa6ee59e5f6d0e7b68770927737c5bf2e8ffc4f43858d381f0e90ef027041ab8ba23'
