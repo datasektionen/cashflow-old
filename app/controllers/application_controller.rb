b6825e359631c2ebcc5cbdccd37e1d566d671509
Martin Frost <frost@ceri.se>
2010-07-17 14:18:41 +0200

    Lagt till lite grundläggande before-filters i application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_filter :cas_filter
+  before_filter :current_user
   protect_from_forgery
   layout 'application'
+  
+  private
+  def cas_filter
+    CASClient::Frameworks::Rails::Filter.filter(self)
+  end
+  
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = PersonSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.person
+  end
+  
 end
