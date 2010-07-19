c06030a357119b48165d0db01d2f3e987f9d18aa
Martin Frost <frost@ceri.se>
2010-07-19 22:00:36 +0200

    config för authlogic_cas. Refs #703.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Cashflow::Application.initialize!
+
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
+CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "https://login.kth.se")
