38d3a8a07b60bda29cf35858c23865f8b352e903
Martin Frost <frost@ceri.se>
2010-09-28 23:44:27 +0200

    testa att fake-logga in i welcome_controller_spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe WelcomeController do
   render_views
+  
+  it "should render a 'log out' link when logged in" do
+    session[:cas_user] = "u1dhz6b0"
+    get :index
+    response.body.should include("Logga ut")
+    
+  end
 end
