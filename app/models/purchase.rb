bb5aef88b7fb4eee013a8a411a779f871ae14663
Martin Frost <frost@ceri.se>
2010-10-27 14:38:09 +0200

    lagt till lite valideringar (presence of :description, :purchased_at)
    satt :person, :person_id som readonly
    
    validera att ett inköp inte gjorts i framtiden.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -7,7 +7,11 @@ class Purchase < ActiveRecord::Base
   belongs_to :updated_by, :class_name => "Person", :foreign_key => "updated_by_id"
   belongs_to :business_unit
   
-  validates_presence_of :person, :created_by, :updated_by, :business_unit
+  validates_presence_of :person, :created_by, :updated_by, :business_unit, :description, :purchased_at
+  
+  validate :cannot_purchase_stuff_in_the_future
+  
+  attr_readonly :person, :person_id
   
   # workflow for Purchase model
   #                                   :keep --> (bookkept) -- :pay --
@@ -51,5 +55,14 @@ class Purchase < ActiveRecord::Base
     state :finalized
     
     state :cancelled
+  end
+  
+  protected
+  
+  def cannot_purchase_stuff_in_the_future
+    if !self.purchased_at.blank? && self.purchased_at > Date.today
+      errors.add(:base, "Du kan inte ha köpt in något i framtiden. Har du uppfunnit en tidsmaskin får du gärna kontakta mig på frost@ceri.se.")
+      errors.add(:purchased_at, "får inte vara senare än dagens datum")
+    end
   end
 end
