d9148aab5576b2a95e493b0766d5942e62727a51
Martin Frost <frost@ceri.se>
2010-10-27 14:33:17 +0200

    små buggfixar. plockat bort role från 'invalid without'-specen eftersom den defaultar till ''. Refs #715.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe Person do
   before(:each) do
-    @person = Person.new
+    @person = Factory(:person)
   end
   
   it "should have a default role of \"\"" do
     @person.role.should == ""
   end
   
-  %w[first_name last_name email ugid login role].each do |attribute|
+  %w[first_name last_name email ugid login].each do |attribute|
     it "should be invalid without a #{attribute}" do
       @person.send("#{attribute}=", nil)
       @person.should be_invalid
@@ -20,7 +20,6 @@ describe Person do
 
   %w[first_name last_name ugid login role].each do |attribute|
     it "should protect attribute #{attribute}" do
-      @person = Factory(:person)
       attr_value = @person.send(attribute)
       @person.update_attributes({attribute.to_sym => "blubb"}).should be_true
       @person.send(attribute).should == attr_value
