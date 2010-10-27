9c3a7e1707bb75a455b93dd3b886adac38c38203
Martin Frost <frost@ceri.se>
2010-10-27 11:10:26 +0200

    lagt till spec för att verifiera att inköp är ogiltiga utan en person. Refs #707 #715.

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Purchase do
+  before(:each) do
+    @purchase = Factory :purchase
+  end
+  
+  it "should be invalid without an owner" do
+    @purchase.person = nil
+    @purchase.should_not be_valid
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
