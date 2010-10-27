0b44ceaa571812ad8c1e28da5c896723d51a49d5
Martin Frost <frost@ceri.se>
2010-10-27 11:03:28 +0200

    Lagt till spec för att verifiera att det inte går att ändra person på en skuld. Refs #711 #715.

--- spec/models/debt_spec.rb
+++ spec/models/debt_spec.rb
@@ -33,10 +33,67 @@ describe Debt do
     @debt.should be_cancelled
   end
   
-  it "should not be cancelable once paid" do
-    @debt.pay!
+  %w[paid bookkept finalized cancelled].each do |state|
+    it "should not be cancelable once #{state}" do
+      @debt.workflow_state = state
+      @debt.save
+      @debt.should be_valid
+      @debt.send("#{state}?").should be_true
+      lambda {@debt.cancel!}.should raise_error(Workflow::NoTransitionAllowed)
+    end
+  end
+  
+  it "should not allow mass update of workflow state" do
+    attrs = {:workflow_state => "foobar"}
+    @debt.update_attributes(attrs)
+    @debt.save
     @debt.should be_valid
-    @debt.should be_paid
-    pending "TODO: validate what transitions are available or something."
+    @debt.workflow_state.should_not == "foobar"
+  end
+  
+  %w[new bookkept].each do |state|
+    it "should be payable if #{state}" do
+      @debt.workflow_state = state
+      @debt.save
+      @debt.should be_valid
+      lambda {@debt.pay!}.should_not raise_error
+    end
+  end
+  
+  %w[new paid].each do |state|
+    it "should be 'bookkeepable' if #{state}" do
+      @debt.workflow_state = state
+      @debt.save
+      @debt.should be_valid
+      lambda {@debt.keep!}.should_not raise_error
+    end
+  end
+  
+  it "should get finalized when paid and bookkept" do
+    @debt.workflow_state = "paid"
+    @debt.save
+    @debt.should be_valid
+    lambda {@debt.keep!}.should_not raise_error
+    @debt.should be_finalized
+    
+    @debt.workflow_state = "bookkept"
+    @debt.save
+    @debt.should be_valid
+    lambda {@debt.pay!}.should_not raise_error
+    @debt.should be_finalized
+  end
+  
+  it "should not ever change owner" do
+    @debt.person.should_not be_nil
+    @debt.should be_valid
+    
+    new_person = Factory :person
+    @debt.update_attributes(:person_id => new_person.id)
+    @debt.save.should be_true
+    @debt.person.should_not ==(new_person)
+
+    @debt.person = new_person
+    @debt.save.should be_true
+    @debt.reload.person.should_not ==(new_person)
   end
 end
