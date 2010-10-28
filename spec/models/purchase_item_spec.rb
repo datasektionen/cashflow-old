e90528a09f5ef0f1786d53f16701d4f46b50c4d5
Martin Frost <frost@ceri.se>
2010-10-28 09:28:02 +0200

    börjat skriva på specs för PurchaseItem. Lagt till specs för att verifiera att en PurchaseItem hör till ett inköp. Refs #707 #715.

--- spec/models/purchase_item_spec.rb
+++ spec/models/purchase_item_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe PurchaseItem do
+  before(:each) do
+    @item = Factory :purchase_item
+  end
+  
+  %w[purchase].each do |relation|
+    it "should belong to a #{relation}" do
+      pending "TODO"
+    end
+  end
+  
   pending "add some examples to (or delete) #{__FILE__}"
 end
