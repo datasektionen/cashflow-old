5b97257cec722e030320fbb7b1009607e4d71942
Martin Frost <frost@ceri.se>
2010-10-31 17:07:16 +0100

    Lagt till kod för att importera användarinformation från LDAP. Refs #706.

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,22 +6,67 @@ class Person < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :login, :email, :ugid
   attr_accessible :email
   
-  
+  # String representation of a user.
+  # Basically the same as the cn column in the LDAP server, that is:
+  # "Firstname Lastname (username)"
   def to_s
-    "%s %s" % [first_name, last_name]
+    "%s %s (%s)" % [first_name, last_name, login]
   end
   
-  # FIXME: write this method
-  # http://www.liveandcode.com/2009/08/30/ldap-pass-through-authentication-with-authlogic-and-activeldap/
-  # rewrite AuthlogicCas::Session#persist_by_cas to use this method for finding people.
-  # load ldap configs from Rails.root / config / ldap.yml using an OpenStruct or something...
-  def find_or_create_from_ldap(kth_ugid)
-    # if we don't already have a user with this ugid, we'll need to import his/her credentials from LDAP
-    unless person = find_by_ugid(kth_ugid)
-      person = Person.new(:ugid => kth_ugid)
-      # do magic ldap stuffsies
-      person.save
+  # search KTH's LDAP server for a user.
+  # This method will return a new Person object if it finds any information from the LDAP server.
+  # The options hash should contain search filters.
+  # The following filters are allowed:
+  # * first_name
+  # * last_name
+  # * ugid
+  # * login
+  # * email
+  # 
+  # If supplied with more than one filter, the search will be more specific (first_name=foo & last_name=bar).
+  # 
+  # Make sure to supply it with specific filters, since it will only return the first user it finds.
+  def self.from_ldap(options = {})
+    filters = {:givenName   => options[:first_name],
+               :sn          => options[:last_name],
+               :ugKthid     => options[:ugid],
+               :ugusername  => options[:login],
+               :mail        => options[:email]}
+    
+    filters.reject! {|k,v| v.blank? }
+    
+    # if we don't have any filters, we won't have to search
+    return nil if filters.empty?
+    
+    # map filters to proper ldap filters,
+    # and join them into a big query
+    filter = filters.map do |k,v|
+      Net::LDAP::Filter.eq(k,v)
+    end.inject {|x,y| x&y }
+    
+    ldap = Net::LDAP.new( :host => Cashflow::Application.settings["ldap_host"], 
+                          :base => Cashflow::Application.settings["ldap_basedn"], 
+                          :port => Cashflow::Application.settings["ldap_port"])
+    
+    ldap.search(:filter => filter) do |user|
+      person = new()
+      person.first_name = user.givenName.first
+      person.last_name = user.sn.first
+      person.login = user.ugusername.first
+      person.ugid = user.ugkthid.first
+      person.email = user.mail.first
+      return person
     end
-    return person
+    return nil
+  end
+  
+  # Utilize Person.from_ldap and save the resulting user.
+  # This is for example used by PersonSessionController in order to make sure that all logged in CAS users have a corresponding Person-object.
+  def self.create_from_ldap(options)
+    if p = from_ldap(options)
+      p.save
+    else
+      nil
+    end
   end
 end
