ea74e30120f9bbd7acc6aa1ff770a737aa7c1510
Martin Frost <frost@ceri.se>
2010-10-31 14:27:04 +0100

    Lagt till en pil från edited till cancelled i workflow-asciigrafen, och lagt till has_many :items till Purchase. Refs #707.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,6 +6,8 @@ class Purchase < ActiveRecord::Base
   belongs_to :created_by, :class_name => "Person", :foreign_key => "created_by_id"
   belongs_to :updated_by, :class_name => "Person", :foreign_key => "updated_by_id"
   belongs_to :business_unit
+
+  has_many :items, :class_name => "PurchaseItem", :dependent => :destroy
   
   validates_presence_of :person, :created_by, :updated_by, :business_unit, :description, :purchased_at
   
@@ -21,9 +23,9 @@ class Purchase < ActiveRecord::Base
   # (new)          :confirm  :edit   :pay   --> (paid) -- :keep --> (finalized)
   #   \ \               \    v
   #    \ :edit     --> (edited)
-  #     \
-  #      \
-  #        :cancel -->  (cancelled)
+  #     \                   \
+  #      \                   \
+  #       ------------------> :cancel -->  (cancelled)
   # 
   workflow do
     state :new do
