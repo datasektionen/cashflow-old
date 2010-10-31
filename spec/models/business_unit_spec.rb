7414efeaefd0d2764a3d637e09db4ac8412c5b23
Martin Frost <frost@ceri.se>
2010-10-31 14:22:54 +0100

    lagt till lite specs för BusinessUnit. kollar så att den har alla relationer rätt och att den inte går att ta bort om den har relaterade inköp. Refs #709 #715

--- spec/models/business_unit_spec.rb
+++ spec/models/business_unit_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe BusinessUnit do
+  before(:each) do
+    @business_unit = Factory :business_unit
+  end
+  
+  %w[name short_name description].each do |attribute|
+    it "should be invalid with a nil #{attribute}" do
+      @business_unit.send("#{attribute}=", nil)
+      @business_unit.should be_invalid
+      @business_unit.errors[attribute.to_sym].should_not be_empty
+    end
+  end
+  
+  it "should have many purchases" do
+    @business_unit.should respond_to(:purchases)
+  end
+  
+  it "should not be deletable if it has any related purchases" do
+    purchase = Factory :purchase, :business_unit => @business_unit
+    
+    business_unit_id = @business_unit.id
+    
+    lambda {@business_unit.destroy}.should raise_error
+    
+    BusinessUnit.find(business_unit_id).should_not be_nil
+  end
+  
   pending "add some examples to (or delete) #{__FILE__}"
 end
