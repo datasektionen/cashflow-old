9148344f26b8efd24cecd9f79e942c10811426be
Martin Frost <frost@ceri.se>
2010-11-11 22:24:08 +0100

    Specs för LDAP-kopplingen. Refs #715 #708 #706.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -25,4 +25,103 @@ describe Person do
       @person.send(attribute).should == attr_value
     end
   end
+  
+  it "should correctly sum the total of all debts" do
+    # make sure it can count the total of one debt.
+    d1 = Factory :debt, :person_id => @person.id
+    @person.reload.total_debt_amount.should == d1.amount
+    
+    # ...and add the sum of two debts together
+    d2 = Factory :debt, :person_id => @person.id
+    @person.reload.total_debt_amount.should == (d1.amount + d2.amount)
+    
+    # ..and not count paid debts
+    d2.pay!
+    @person.reload.total_debt_amount.should == d1.amount
+    
+    # also, do count bookkept debts, but not paid ones
+    d1.keep!
+    d3 = Factory :debt, :person_id => @person.id 
+    @person.reload.total_debt_amount.should == (d1.amount + d3.amount)
+    
+    # if we cancel one debt, it should not be counted
+    d3.cancel!
+    @person.reload.total_debt_amount.should == d1.amount
+    
+    # and when no unpaid, uncancelled debts remain, the total debt sum should be zero
+    d1.pay!
+    @person.reload.total_debt_amount.should == 0
+  end
+  
+  it "should corretly sum the total of all purchases" do
+    p1 = Factory :purchase, :person_id => @person.id
+    @person.reload.total_purchased_amount.should == p1.total
+    
+    p2 = Factory :purchase, :person_id => @person.id
+    @person.reload.total_purchased_amount.should == (p1.total + p2.total)
+    
+    p1.cancel!
+    @person.reload.total_purchased_amount.should == p2.total
+    
+    p2.confirm!
+    p3 = Factory :purchase, :person_id => @person.id
+    @person.reload.total_purchased_amount.should == (p2.total + p3.total)
+    
+    p2.edit!
+    @person.reload.total_purchased_amount.should == (p2.total + p3.total)
+    
+    p2.confirm!
+    p2.keep!
+    @person.reload.total_purchased_amount.should == (p2.total + p3.total)
+    
+    p3.confirm!
+    p3.pay!
+    @person.reload.total_purchased_amount.should == (p2.total)
+    
+    p2.pay!
+    @person.reload.total_purchased_amount.should == 0
+  end
+  
+  it "should have a string representation matching its LDAP cn" do
+    @person.cn.should == "#{@person.first_name} #{@person.last_name} (#{@person.login})"
+    @person.to_s.should == @person.cn
+  end
+
+  # create an LDAP connection for these specs to wor
+  describe "LDAP connection" do
+    it "should correctly fetch information from LDAP" do
+      begin
+        # make this spec search by:
+        # * first_name, last_name
+        # * login
+        # * ugid
+        # * email
+        local_config = YAML.load(File.read("#{Rails.root}/config/local.yml"))
+        name_search_params  = {:first_name => local_config[:yourself][:first_name], :last_name => local_config[:yourself][:last_name]}
+        email_search_params = {:email => local_config[:yourself][:email]}
+        ugid_search_params  = {:ugid => local_config[:yourself][:ugid]}
+        login_search_params = {:login => local_config[:yourself][:login]}
+
+        Person.from_ldap(name_search_params).should_not be_nil 
+        Person.from_ldap(email_search_params).should_not be_nil 
+        Person.from_ldap(ugid_search_params).should_not be_nil 
+        Person.from_ldap(login_search_params).should_not be_nil 
+        
+        # TODO: verify a few more things than just searching... like some things should return nil (ugid search of "azovwemzavwmdzvway" for instance)
+      rescue Net::LDAP::LdapError => e
+        pending "UNABLE TO CONNECT TO LDAP SERVER"
+      end
+      
+    end
+  
+    it "should correctly create users from an LDAP search" do
+      begin
+        local_config = YAML.load(File.read("#{Rails.root}/config/local.yml"))
+        Person.create_from_ldap(:ugid => local_config[:yourself][:ugid])
+      rescue Net::LDAP::LdapError => e
+        pending "UNABLE TO CONNECT TO LDAP SERVER"
+      end
+      
+    end
+  end
 end
