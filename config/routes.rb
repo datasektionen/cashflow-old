09bab55ac72e5646069facf597c28b78d42ba613
Martin Frost <frost@ceri.se>
2010-11-12 15:12:01 +0100

    lagt till ett par routes för inköp och skulder. Refs #707 #711.

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,21 @@
-Cashflow::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
-  
+Cashflow::Application.routes.draw do  
   resources :product_types
   resources :business_units
+  resources :purchases do
+    resources :items, :controller => "purchase_items"
+  end
+  resources :debts
+  
   resources :people
+  # TODO: borde vi ha kanske nästla purchases och debts under people också?
+  # det skulle kinda make sense för mobil-appen, om man inte vill rendera all den infon på people#show
+  # Det skulle också vara vettigt för icke-admin/kassör/revisor, då de ändå bara ska kunna se sina egna inköp/skulder.
+  # resources :people do
+  #   resources :debts
+  #   resources :purchases do
+  #     resources :items, :controller => "purchase_items"
+  #   end
+  # end
   
   match 'login', :to => "person_sessions#new"
   match 'logout', :to => "person_sessions#destroy"
