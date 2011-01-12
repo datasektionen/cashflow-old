b2dda7ef46a4b4e121d5eac42219959f3c4fa984
Martin Frost <frost@ceri.se>
2011-01-13 00:19:49 +0100

    Fixat färdigt lite saker med mailstuffet. Nu borde det vara klart för att lalla runt lite i stagingmiljön. Fixes #898.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,12 +1,16 @@
 class Notifier < ActionMailer::Base
+  add_template_helper(ApplicationHelper)
   default :from => "noreply@d.ths.kth.se"
+  default_url_options[:host] = Cashflow::Appliation.settings["default_host"]
 
   def purchase_approved(purchase, administrator)
     @purchase = purchase
     @administrator = administrator
     mail(:to => @purchase.person.email, 
          :subject => "Inköp godkänt", 
-         :cc => @administrator.email) do |format|
+         :cc => @administrator.email,
+         :reply_to => @administrator.email
+        ) do |format|
       format.text
     end
   end
@@ -16,7 +20,9 @@ class Notifier < ActionMailer::Base
     @administrator = administrator
     mail(:to => @purchase.person.email, 
          :subject => "Inköp avslaget", 
-         :cc => @administrator.email) do |format|
+         :cc => @administrator.email,
+         :reply_to => @administrator.email
+        ) do |format|
       format.text
     end
   end
@@ -26,7 +32,9 @@ class Notifier < ActionMailer::Base
     @administrator = administrator
     mail(:to => @purchase.person.email, 
          :subject => "Inköp utbetalat", 
-         :cc => @administrator.email) do |format|
+         :cc => @administrator.email,
+         :reply_to => @administrator.email
+        ) do |format|
       format.text
     end
   end
@@ -36,7 +44,9 @@ class Notifier < ActionMailer::Base
     @administrator = administrator
     mail(:to => @debt.person.email, 
          :subject => "Skuld inlagd", 
-         :cc => @administrator.email) do |format|
+         :cc => @administrator.email,
+         :reply_to => @administrator.email
+        ) do |format|
       format.text
     end
   end
@@ -46,7 +56,9 @@ class Notifier < ActionMailer::Base
     @administrator = administrator
     mail(:to => @debt.person.email, 
          :subject => "Skuld betalad", 
-         :cc => @administrator.email) do |format|
+         :cc => @administrator.email,
+         :reply_to => @administrator.email
+        ) do |format|
       format.text
     end
   end
