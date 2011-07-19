025658efba826ff30994b723c4c430b76bc2258d
Victor Koronen <koronen@kth.se>
2011-07-19 23:21:32 +0200

    Typo

--- app/models/debt_observer.rb
+++ app/models/debt_observer.rb
@@ -2,18 +2,18 @@ class DebtObserver < ActiveRecord::Observer
   def after_create(debt)
     person = debt.author
     debt.logger.info("New debt created by #{person}!")
-    Notifier.debt_created(@debt, person).deliver
+    Notifier.debt_created(debt, person).deliver
   end
 
   def after_cancel(debt)
     person = Person.find(debt.versions.last.whodunnit)
     debt.logger.info("Debt cancelled by #{person}!")
-    Notifier.debt_cancelled(@debt, person).deliver
+    Notifier.debt_cancelled(debt, person).deliver
   end
 
   def after_pay(debt)
     person = Person.find(debt.versions.last.whodunnit)
     debt.logger.info("Debt paid by #{person}!")
-    Notifier.debt_paid(@debt, person).deliver
+    Notifier.debt_paid(debt, person).deliver
   end
 end
--- app/models/purchase_observer.rb
+++ app/models/purchase_observer.rb
@@ -2,18 +2,18 @@ class PurchaseObserver < ActiveRecord::Observer
   def after_confirm(purchase)
     person = purchase.updated_by
     purchase.logger.info("Purchase confirmed by #{person}!")
-    Notifier.purchase_approved(@purchase, person).deliver
+    Notifier.purchase_approved(purchase, person).deliver
   end
 
   def after_pay(purchase)
     person = purchase.updated_by
     purchase.logger.info("Purchase paid by #{person}!")
-    Notifier.purchase_paid(@purchase, person).deliver
+    Notifier.purchase_paid(purchase, person).deliver
   end
 
   def after_cancel(purchase)
     person = purchase.updated_by
     purchase.logger.info("Purchase cancelled by #{person}!")
-    Notifier.purchase_denied(@purchase, person).deliver
+    Notifier.purchase_denied(purchase, person).deliver
   end
 end
