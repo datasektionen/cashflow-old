2b9e89da96273d8d6b82346cb30fd44a48861ef4
Andreas Tarandi <torandi@gmail.com>
2011-08-09 22:28:52 +0200

    Budget posts can now be editet

--- app/models/budget_post.rb
+++ app/models/budget_post.rb
@@ -2,6 +2,8 @@ class BudgetPost < ActiveRecord::Base
   has_many :budget_rows
   belongs_to :business_unit
 
+  accepts_nested_attributes_for :budget_rows
+
   scope :business_unit, lambda { |bu| where(:business_unit_id => bu) }
 
   after_create :create_rows
