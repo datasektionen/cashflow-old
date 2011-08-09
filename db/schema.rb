5f498f4a2261d28d00fc51c4e83338cd197ceb58
Martin Frost <frost@ceri.se>
2011-08-09 19:36:27 +0200

    schema bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110809140238) do
+ActiveRecord::Schema.define(:version => 20110809162342) do
 
   create_table "budget_posts", :force => true do |t|
     t.integer "business_unit_id", :null => false
