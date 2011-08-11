datasektionen/cashflow-old

commit 49beb4c3c9c8bbd5ba9f419b6df746e90f58e770

Använd find_by_business_unit istället

app/views/purchases/_form.html.haml
@@ -4,7 +4,7 @@
   = form.inputs :name => "Beskrivning" do
     = form.input :purchased_at, :as => :datepicker
     = form.input :business_unit, :collection => BusinessUnit.all
-    = form.input :budget_post, :collection => BudgetPost.business_unit(@purchase.try(:business_unit)), :include_blank=>false
+    = form.input :budget_post, :collection => BudgetPost.find_by_business_unit(@purchase.try(:business_unit)), :include_blank=>false
     = form.input :description, :as => :text
   
   = form.inputs :name => "Inköpsdelar" do

app/models/budget_post.rb
@@ -4,8 +4,6 @@ class BudgetPost < ActiveRecord::Base
 
   accepts_nested_attributes_for :budget_rows
 
-  scope :business_unit, lambda { |bu| where(:business_unit_id => bu) }
-
   after_create :create_rows
 
   def to_s

commit ae3a8bf386e1ccbd2ad92dfcfd526a7a06e57a35

Fixes errors introduced in 49beb4c

app/models/business_unit.rb
@@ -2,6 +2,7 @@ class BusinessUnit < ActiveRecord::Base
   validates_presence_of :name, :short_name, :description
   
   has_many :purchases, :dependent => :restrict
+  has_many :budget_posts
 
   has_friendly_id :short_name
   

app/views/purchases/_form.html.haml
@@ -4,7 +4,7 @@
   = form.inputs :name => "Beskrivning" do
     = form.input :purchased_at, :as => :datepicker
     = form.input :business_unit, :collection => BusinessUnit.all
-    = form.input :budget_post, :collection => BudgetPost.find_by_business_unit(@purchase.try(:business_unit)), :include_blank=>false
+    = form.input :budget_post, :collection => @purchase.try(:business_unit).try(:budget_posts), :include_blank=>false
     = form.input :description, :as => :text
   
   = form.inputs :name => "Inköpsdelar" do
@@ -18,7 +18,7 @@
 - ActiveRecord::Base.include_root_in_json = false
 
 :javascript
-  budget_posts = #{ bp = Hash.new; BusinessUnit.all.each { |bu| bp[bu.id] = BudgetPost.business_unit(bu) } ; bp.to_json  }
+  budget_posts = #{ bp = Hash.new; BusinessUnit.all.each { |bu| bp[bu.id] = bu.budget_posts } ; bp.to_json  }
   $("#purchase_business_unit_id").change(function(){
     var new_html = ""
     $.each(budget_posts[$("#purchase_business_unit_id").val()],function(index, bp) {