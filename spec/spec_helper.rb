464c430ce149590f0121cbfabf035e384931f51b
Martin Frost <frost@ceri.se>
2011-09-11 01:54:07 +0200

    Removed fixture_path from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,22 +13,9 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 local_config = YAML.load(File.read("#{Rails.root}/config/local.yml"))
 ActiveRecord::Observer.disable_observers
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-  
 
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
