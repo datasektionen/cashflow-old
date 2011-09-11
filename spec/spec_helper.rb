67c73574e7c693e4dfd564f0f9b08007e19d26dc
Martin Frost <frost@ceri.se>
2011-09-11 16:57:19 +0200

    Added test helpers to view specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,5 +19,7 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
+  config.include Devise::TestHelpers, :type => :view
+  config.extend ControllerMacros, :type => :view
 end
 
