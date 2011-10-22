a8e3f2b47c9c039d530cdfb4d3416710f9224152
Martin Frost <frost@ceri.se>
2011-10-22 17:55:06 +0200

    got rid of a bunch of unnecessary database calls

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,7 @@ class PurchasesController < ApplicationController
   # GET /purchases
   # GET /purchases.xml
   def index
-    @purchase = Purchase.all
+    @purchases = Purchase.joins(:person, :items, :budget_post => :business_unit).includes(:person, :items, :budget_post => :business_unit)
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @purchases }
