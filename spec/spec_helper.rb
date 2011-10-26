5ce11e3787a457ae6a404626b28720eca667c80e
Martin Frost <frost@ceri.se>
2011-10-26 18:29:11 +0200

    Added fail_fast and database_cleaner to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/rails'
 require 'controller_macros'
 require 'formtastic'
 
+require 'database_cleaner'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
@@ -15,11 +17,25 @@ local_config = YAML.load(File.read("#{Rails.root}/config/local.yml"))
 ActiveRecord::Observer.disable_observers
 RSpec.configure do |config|
   config.mock_with :rspec
-
+  config.fail_fast = true
   config.use_transactional_fixtures = true
 
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
   config.include Devise::TestHelpers, :type => :view
   config.extend ControllerMacros, :type => :view
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
