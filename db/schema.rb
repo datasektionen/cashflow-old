c26eeef83d9debf388eed94b9f79b4701827f250
Martin Frost <frost@ceri.se>
2011-10-26 23:29:56 +0200

    Added email to business unit form.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -30,7 +31,7 @@ ActiveRecord::Schema.define(:version => 20110909142954) do
     t.boolean  "active",      :default => true, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email"
+    t.string   "email",       :default => "",   :null => false
   end
 
   create_table "debts", :force => true do |t|
