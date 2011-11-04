0eaf7ed7fd7eed9c7697418c369b07f6c7fbb5bc
Andreas Tarandi <torandi@gmail.com>
2011-11-05 00:56:38 +0100

    Adds test for previosly added code

--- spec/models/mage/voucher_spec.rb
+++ spec/models/mage/voucher_spec.rb
@@ -13,4 +13,20 @@ describe Mage::Voucher do
     v.attributes[:voucher_rows].should == nil
     v.accounting_date.should == "2011-11-11"
   end
+
+  it "should convert purchase into voucher correctly" do
+    person = Factory :person
+    PaperTrail.whodunnit = person.id.to_s
+    p = Factory :purchase, workflow_state: :paid
+    pi = Factory.build(:purchase_item)
+    pi.purchase = p
+    pi.save
+
+    v = Mage::Voucher.from_purchase(p,"M")
+    v.accounting_date.should == p.purchased_at
+    v.authorized_by.should == p.confirmed_by.ugid
+    v.material_from.should == p.person.ugid
+    v.organ.should == p.budget_post.business_unit.mage_number
+    v.title.should == p.description
+  end
 end
