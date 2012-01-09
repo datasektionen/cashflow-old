f84c263b6c98ae1d5fa33bd0b7f8041d1de505bd
Martin Frost <frost@ceri.se>
2012-01-10 00:49:40 +0100

    Removed database calls from budget_rows_controller

--- spec/controllers/budget_rows_controller_spec.rb
+++ spec/controllers/budget_rows_controller_spec.rb
@@ -12,7 +12,6 @@ describe BudgetRowsController do
     @ability.extend(CanCan::Ability)
     @ability.can :access, :all
     @ability.can :manage, :all
-    #@controller.stub(:current_ability).and_return(@ability)
   end
 
   def valid_attributes
@@ -28,14 +27,15 @@ describe BudgetRowsController do
       budget_row = mock_budget_row
       BudgetRow.stub(:year).with(@year).and_return([budget_row])
       get :index, budget_id: @year
-      assigns(:budget_rows).should eq([budget_row])
+      assigns(:budget_rows).should == [budget_row]
     end
   end
 
   describe "GET show" do
     it "assigns the requested budget_row as @budget_row" do
-      budget_post = Factory :budget_post
-      budget_row = budget_post.row(@year)
+      budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+      BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+
       get :show, budget_id: @year, id: budget_row.id
       assigns(:budget_row).should eq(budget_row)
     end
@@ -43,8 +43,9 @@ describe BudgetRowsController do
 
   describe "GET edit" do
     it "assigns the requested budget_row as @budget_row" do
-      budget_post = Factory :budget_post
-      budget_row = budget_post.row(@year)
+      budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+      BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+
       get :edit, id: budget_row.id, budget_id: @year
       assigns(:budget_row).should eq(budget_row)
     end
@@ -53,22 +54,27 @@ describe BudgetRowsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested budget_row" do
-        budget_post = Factory :budget_post
-        budget_row = budget_post.row(@year)
-        BudgetRow.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+        budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+        BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+        budget_row.should_receive(:update_attributes).with({'these' => 'params'})
+
         put :update, budget_id: @year, id: budget_row.id, budget_row: {'these' => 'params'}
       end
 
       it "assigns the requested budget_row as @budget_row" do
-        budget_post = Factory :budget_post
-        budget_row = budget_post.row(@year)
+        budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+        BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+        budget_row.stub(:update_attributes)
+
         put :update, budget_id: @year, id: budget_row.id, budget_row: valid_attributes
         assigns(:budget_row).should eq(budget_row)
       end
 
       it "redirects to the budget_row" do
-        budget_post = Factory :budget_post
-        budget_row = budget_post.row(@year)
+        budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+        BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+        budget_row.stub(:update_attributes).and_return(true)
+
         put :update, budget_id: @year, id: budget_row.id, budget_row: valid_attributes
         response.should redirect_to(budget_row_path(@year,budget_row))
       end
@@ -76,19 +82,19 @@ describe BudgetRowsController do
 
     describe "with invalid params" do
       it "assigns the budget_row as @budget_row" do
-        budget_post = Factory :budget_post
-        budget_row = budget_post.row(@year)
-        # Trigger the behavior that occurs when invalid params are submitted
-        BudgetRow.any_instance.stub(:save).and_return(false)
+        budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+        BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+        budget_row.stub(:update_attributes).and_return(false)
+
         put :update, budget_id: @year, id: budget_row.id, budget_row: {}
         assigns(:budget_row).should eq(budget_row)
       end
 
       it "re-renders the 'edit' template" do
-        budget_post = Factory :budget_post
-        budget_row = budget_post.row(@year)
-        # Trigger the behavior that occurs when invalid params are submitted
-        BudgetRow.any_instance.stub(:save).and_return(false)
+        budget_row = mock_budget_row(id: "17", budget_post: double('budget_post', name: "post"))
+        BudgetRow.stub(:find).with(budget_row.id).and_return(budget_row)
+        budget_row.stub(:update_attributes).and_return(false)
+
         put :update, budget_id: @year, id: budget_row.id, budget_row: {}
         response.should render_template("edit")
       end
