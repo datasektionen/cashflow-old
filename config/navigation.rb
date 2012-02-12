15a1d9b393111acb6efd342d78cf616a698a3f6c
Andreas Falk <adde.falk@gmail.com>
2012-02-12 14:30:32 +0100

    Remove the manual highlighting of active menu items. It's done automatically

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,7 +1,7 @@
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-    primary.item :purchases, I18n.t('navigation.purchases'), purchases_path, :highlights_on => /\/purchases/ do |sub|
+    primary.item :purchases, I18n.t('navigation.purchases'), purchases_path do |sub|
       sub.item :all_purchases, I18n.t('navigation.all_purchases'), purchases_path
       if can?(:manage, Purchase)
         sub.item :confirmed_purchases, I18n.t('navigation.confirmed_purchases'), confirmed_purchases_path
@@ -9,39 +9,39 @@ SimpleNavigation::Configuration.run do |navigation|
       sub.item :new_purchase, I18n.t('navigation.new_purchase'), new_purchase_path
     end
 
-    primary.item :debts, I18n.t('navigation.debts'), debts_path, :highlights_on => /\/debts/ do |sub|
+    primary.item :debts, I18n.t('navigation.debts'), debts_path do |sub|
       sub.item :all_debts, I18n.t('navigation.all_debts'), debts_path
       if can?(:create, Debt)
         sub.item :new_debt, I18n.t('navigation.new_debt'), new_debt_path
       end
     end
 
-    primary.item :budgets, I18n.t('navigation.budgets'), budget_path(:id => Time.now.year), :highlights_on => /\/budget\/\d{4}/ do |secondary|
+    primary.item :budgets, I18n.t('navigation.budgets'), budget_path(:id => Time.now.year) do |secondary|
       BudgetPost.all_years.each { |year|
         secondary.item :"budget_year_#{year}", year, budget_path(id: year)
       }
     end
 
     if current_user.is?(:admin)
-      primary.item(:people, I18n.t('navigation.people'), people_path, :highlights_on => /\/people/)
+      primary.item(:people, I18n.t('navigation.people'), people_path)
     end
 
     if can?(:manage, BusinessUnit)
-      primary.item :business_units, I18n.t('navigation.business_units'), business_units_path, :highlights_on => /\/business_units/ do |sub|
+      primary.item :business_units, I18n.t('navigation.business_units'), business_units_path do |sub|
         sub.item :all_business_units, I18n.t('navigation.all_business_units'), business_units_path
         sub.item :new_business_unit, I18n.t('navigation.new_business_unit'), new_business_unit_path
       end
     end
 
     if can?(:manage, ProductType)
-      primary.item :product_types, I18n.t('navigation.product_types'), product_types_path, :highlights_on => /\/product_types/ do |sub|
+      primary.item :product_types, I18n.t('navigation.product_types'), product_types_path do |sub|
         sub.item :all_product_types, I18n.t('navigation.all_product_types'), product_types_path
         sub.item :new_product_type, I18n.t('navigation.new_product_type'), new_product_type_path
       end
     end
 
     if can?(:manage, BudgetPost)
-      primary.item :budget_posts, I18n.t('navigation.budget_posts'), budget_posts_path, :highlights_on => /\/budget_posts/ do |sub|
+      primary.item :budget_posts, I18n.t('navigation.budget_posts'), budget_posts_path do |sub|
         sub.item :all_budget_posts, I18n.t('navigation.all_budget_posts'), budget_posts_path
         sub.item :new_budget_post, I18n.t('navigation.new_budget_post'), new_budget_post_path
       end
