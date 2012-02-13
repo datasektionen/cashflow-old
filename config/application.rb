6c07f22581fa843e561efabaa226a36e4a2b686c
Andreas Falk <adde.falk@gmail.com>
2012-02-14 00:06:18 +0100

    Update config for loading navigation renderers to work with rails 3.1

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,9 @@ module Cashflow
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
-    config.load_paths += %W( #{RAILS_ROOT}/app/navigation_renderers )
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W( Rails.root.join('/app/navigation_renderers') )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
