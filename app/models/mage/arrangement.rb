b22c6ab532883132e6eb99a1058e2e2389b36b4d
Andreas Tarandi <torandi@gmail.com>
2012-03-15 21:27:49 +0100

    Make all list for a year only

--- app/models/mage/arrangement.rb
+++ app/models/mage/arrangement.rb
@@ -1,2 +1,13 @@
 class Mage::Arrangement < Mage::Base
+  def self.all(year)
+      res = Mage::ApiCall.call("/#{table_name.pluralize}/#{year}.json",nil,{}, :get)
+      p = parse_result(res)
+      if p
+        return p.map do |item|
+          self.new(item)
+        end
+      else
+        false
+      end 
+  end
 end
