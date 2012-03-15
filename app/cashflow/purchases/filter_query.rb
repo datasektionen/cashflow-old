6019db4a55342faae6ed645fc7e329d671dc0870
Victor Koronen <koronen@kth.se>
2012-03-15 11:29:22 +0100

    Only filter allowed parameters

--- app/cashflow/purchases/filter_query.rb
+++ app/cashflow/purchases/filter_query.rb
@@ -1,11 +1,14 @@
 module Cashflow
   module Purchases
     class FilterQuery
+      FILTERED_PARAMS = [:workflow_state, :person_id, business_unit_id: :budget_post]
+      FILTETED_PARAM_SYMS = FILTERED_PARAMS.map{|p| p.is_a?(Hash) ? p.keys : p }.flatten
+
       attr_reader :filters
 
       def initialize(filter = {})
         filter ||= {}
-        filter.delete_if {|key, value| value.nil? || value.empty? }
+        filter.delete_if {|key, value| !FILTETED_PARAM_SYMS.include?(key.to_sym) || value.nil? || value.empty? }
 
         @filters = filter
       end
