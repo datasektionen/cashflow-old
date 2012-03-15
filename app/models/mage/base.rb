b65ee80594aed7042f1da835c2fca4845dbb02b1
Andreas Tarandi <torandi@gmail.com>
2012-03-15 18:48:38 +0100

    More documentations

--- app/models/mage/base.rb
+++ app/models/mage/base.rb
@@ -3,17 +3,30 @@ class Mage::Base
   @@after_initialize=[]
   @@before_initialize=[]
 
+  ##
+  # Initialize an object with the specified attributes
   def initialize(attr={})
     @attr = {}
+    #Run before initialize hooks
     @@before_initialize.each { |c| self.send(c) }
+
+    # If data is stored in subarray under table name, extract it 
+    # Ex:
+    # attr[vouchers] = {:id=>0, :foo=>'bar', :baz=>'foobar'} 
+    # We then need to extract the hash under vouchers
     attr = attr[self.class.table_name] if attr[self.class.table_name]
     attr = attr[self.class.table_name.to_sym] if attr[self.class.table_name.to_sym]
+
+    # Convert all keys to symbol and save internally
     attr.each do |key, val|
       @attr[key.to_sym] = val
     end
+
+    #Run after initialize hooks
     @@after_initialize.each { |c| call(c) }
   end
 
+  # Handle getters, setters and ?-methods
   def method_missing(method,*args)
     if method.match /([^=?]+)([=?])?/
       index = $1.to_sym
@@ -34,11 +47,13 @@ class Mage::Base
   end
 
   # Pushes this model to mage
+  # Note that this always creates a new item.
   def push(current_person)
     res = Mage::ApiCall.call("/#{self.class.table_name.pluralize}/#{@@create_action}.json",current_person,{self.class.table_name.to_sym=>attributes}, :post)
     return self.class.parse_result(res,self) && true # Make boolean
   end
 
+  # Returns all objects of this type
   def self.all
     res = Mage::ApiCall.call("/#{table_name.pluralize}.json",nil,{}, :get)
     p = parse_result(res)
@@ -51,6 +66,7 @@ class Mage::Base
     end 
   end
 
+  # Finds a model by id in mage and returns that model
   def self.find(id)
     res = Mage::ApiCall.call("/#{table_name.pluralize}/#{id}.json", nil, {}, :get)
     p = parse_result(res)
@@ -82,6 +98,9 @@ protected
     end
   end
 
+  # Returns the table name for this model in mage
+  # Converts Mage::NameInCamelCase to name_in_camel_case
+  # If this model is not in Mage namespace it returns false
   def self.table_name
     if name.match /Mage::(.+)/
       return $1.underscore
@@ -90,14 +109,17 @@ protected
     end
   end
 
+  # Change which action is used to create in the api
   def self.create_action(val)
     @@create_action = val
   end
 
+  # Add hook to run after initialization
   def self.after_initialize(method)
     @@after_initialize << method
   end
 
+  # Add hook to run before initialization
   def self.before_initialize(method)
     @@before_initialize << method
   end
