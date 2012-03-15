d7d17faf724ff5f270dcf799233719ba563c660a
Andreas Tarandi <torandi@gmail.com>
2012-03-16 00:36:01 +0100

    Add custom all method to account

--- app/models/mage/account.rb
+++ app/models/mage/account.rb
@@ -1,2 +1,15 @@
 class Mage::Account < Mage::Base
+  ##
+  # Use custom all method that limits per year
+  def self.all(year)
+      res = Mage::ApiCall.call("/activity_years/#{year}/accounts.json",nil,{}, :get)
+      p = parse_result(res)
+      if p
+        return p.map do |item|
+          self.new(item)
+        end
+      else
+        false
+      end 
+  end
 end
--- app/models/mage/arrangement.rb
+++ app/models/mage/arrangement.rb
@@ -1,4 +1,6 @@
 class Mage::Arrangement < Mage::Base
+  ##
+  # Use custom all method that limits per year
   def self.all(year)
       res = Mage::ApiCall.call("/#{table_name.pluralize}/#{year}.json",nil,{}, :get)
       p = parse_result(res)
