12fe294a60b421aa7be85e0f2b55e4b979bcc879
Andreas Tarandi <torandi@gmail.com>
2012-03-16 01:43:01 +0100

    Adds support for organ_filter in arrangement.all

--- app/models/mage/arrangement.rb
+++ app/models/mage/arrangement.rb
@@ -1,15 +1,17 @@
 class Mage::Arrangement < Mage::Base
   ##
   # Use custom all method that limits per year
-  def self.all(year)
-      res = Mage::ApiCall.call("/#{table_name.pluralize}/#{year}.json",nil,{}, :get)
-      p = parse_result(res)
-      if p
-        return p.map do |item|
-          self.new(item)
-        end
-      else
-        false
-      end 
+  # takes optional param organ_number
+  def self.all(year, organ_number=nil)
+    params = organ_number ? {:organ_number=>organ_number} : {}
+    res = Mage::ApiCall.call("/#{table_name.pluralize}/#{year}.json",nil,params, :get)
+    p = parse_result(res)
+    if p
+      return p.map do |item|
+        self.new(item)
+      end
+    else
+      false
+    end 
   end
 end
