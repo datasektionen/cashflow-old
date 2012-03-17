478aa566ae00f9cd66a616f06498148f5e9c13fb
Martin Frost <frost@ceri.se>
2012-03-17 01:42:12 +0100

    Validate range of PurchaseItem#amount. Fixes #69.

--- app/models/purchase_item.rb
+++ app/models/purchase_item.rb
@@ -3,4 +3,5 @@ class PurchaseItem < ActiveRecord::Base
   belongs_to :product_type
   
   validates_presence_of :amount, :product_type
+  validates_numericality_of :amount, greater_than_or_equal_to: 0.1, less_than_or_equal_to: 100_000.0
 end
