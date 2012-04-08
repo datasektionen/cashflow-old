d7e33d355c2fe01862e55a6762ec8a88a8bd4fbe
Victor Koronen <koronen@kth.se>
2012-04-08 17:40:21 +0200

    Add date search form elements

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module PurchasesHelper
   def budget_posts_for_purchase(purchase)
     purchase.budget_post.blank? ? nil : purchase.business_unit.budget_posts
@@ -14,5 +16,16 @@ module PurchasesHelper
   def filter_select_tag name, collection, value_method, text_method, placeholder, options
     options = {:'data-placeholder' => placeholder}.merge(options)
     select_tag "filter[#{name}]", options_from_collection_for_select(collection, value_method, text_method, params[:filter].try(:[], name)), options
+  end
+
+  def filter_date_range_tags name
+    content_tag :div, :id => "purchase_#{name}_filter" do
+      content = ""
+      content += label_tag "filter[#{name.to_s}_from]", 'Från'
+      content += text_field_tag "filter[#{name.to_s}_from]", params['filter'].try(:[], "#{name.to_s}_from"), :class => 'datepicker', :placeholder => 'Välj ett startdatum'
+      content += label_tag "filter[#{name.to_s}_to]", 'till och med'
+      content += text_field_tag "filter[#{name.to_s}_to]", params['filter'].try(:[], "#{name.to_s}_to"), :class => 'datepicker', :placeholder => 'Välj ett slutdatum'
+      content.html_safe
+    end
   end
 end
