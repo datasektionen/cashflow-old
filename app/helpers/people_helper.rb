ee3962c2231f727ad8d8a3b9b35906764a8c4f5d
Martin Frost <frost@ceri.se>
2012-08-21 21:18:28 +0200

    added xfinger image to people/show

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,5 @@
 module PeopleHelper
+  def xfinger_image(person)
+    image_tag("http://www.csc.kth.se/hacks/new/xfinger/image.php?user=#{person.login}", class: "xfinger", alt: "xfinger", title: "xfinger")
+  end
 end
