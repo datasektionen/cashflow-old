ade17585f5b4a5384b681e79faae122d885016f4
Martin Frost <frost@ceri.se>
2014-12-16 09:46:50 +0100

    removed unimplemented test files

--- spec/requests/budget_rows_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe 'BudgetRows' do
-  describe 'GET /budget_rows' do
-  end
-end
--- spec/requests/purchases_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'Purchases' do
-  describe 'GET /purchases' do
-    xit 'works! (now write some real specs)' do
-      get purchases_path
-    end
-  end
-end
