11d8605b2ed0aede80726064a47d3e2c8a7971ca
Martin Frost <frost@ceri.se>
2014-12-16 17:08:23 +0100

    fix failing budget post scenarios

--- features/step_definitions/budget_post_steps.rb
+++ features/step_definitions/budget_post_steps.rb
@@ -3,28 +3,29 @@ Given /^a budget post exists without any purchases$/ do
 end
 
 Given /^a budget post exists with a bunch of purchases$/ do
-  Given 'a budget post exists without any purchases'
   Given 'a purchase'
+  @budget_post = @purchase.budget_post
+  # Given 'a budget post exists without any purchases'
 end
 
-When /^I try to delete it$/ do
+When /^I try to delete the budget post$/ do
   visit('/budget_posts')
-  within(:xpath, "//form[@action='/budget_posts/#{@budget_post.id}']") do
-    click_on('Ta bort')
-    page.driver.browser.switch_to.alert.accept
-  end
+  href = "/budget_posts/#{@budget_post.id}"
+  link = page.all(:xpath, "//a[@href='#{href}' and @data-method='delete']")[0]
+  link.click
+  page.driver.browser.switch_to.alert.accept
 end
 
-Then /^it should not be deleted$/ do
-  pending('TODO: fix mysql wait bug')
-  BudgetPost.exists?(@budget_post.id).should be_true
+Then(/^the budget post should still exist$/) do
+  page.should have_content(@budget_post.name)
 end
 
 Then /^I should get a message saying so$/ do
   page.should have_content('kunde inte tas bort')
 end
 
-Then /^it should be deleted$/ do
+Then /^the budget post should be deleted$/ do
   page.should have_content('Budgetpost borttagen')
+  # page.should have_no_content(@budget_post.name)
   BudgetPost.exists?(@budget_post.id).should be_false
 end
