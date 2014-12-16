887eb7ed5de7ed5ab13e68678eff6011e11fd1da
Martin Frost <frost@ceri.se>
2014-12-17 00:14:42 +0100

    Removed a lot of duplication in Notifier
    Also, added tests for notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -4,89 +4,48 @@ class Notifier < ActionMailer::Base
   default cc: 'kassor@d.kth.se'
   default_url_options[:host] = Cashflow::Application.settings['default_host']
 
-  def purchase_created(purchase)
-    @purchase = purchase
-    @administrator = purchase.last_updated_by
-    mail(to: @purchase.person.email,
-         subject: I18n.t('mailers.notifier.purchase_created'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
-    end
-  end
-
-  def purchase_approved(purchase)
-    @purchase = purchase
-    @administrator = purchase.last_updated_by
-    mail(to: @purchase.person.email,
-         subject: I18n.t('mailers.notifier.purchase_approved'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
-    end
-  end
-
-  def purchase_denied(purchase)
-    @purchase = purchase
-    @administrator = purchase.last_updated_by
-    mail(to: @purchase.person.email,
-         subject: I18n.t('mailers.notifier.purchase_denied'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
+  %w[created approved denied].each do |method|
+    name = "purchase_#{method}"
+    define_method name do |purchase|
+      @purchase = purchase
+      @administrator = purchase.last_updated_by
+      notify(mail_header_params(purchase, name))
     end
   end
 
   def purchase_paid(purchase)
     @purchase = purchase
     @administrator = purchase.last_updated_by
-    options = {
-      to: @purchase.person.email,
-      subject: I18n.t('mailers.notifier.purchase_paid'),
-      reply_to: @administrator.email
+    options = mail_header_params(purchase, 'purchase_paid')
+    options.store(:cc, purchase.business_unit.email || @administrator.email)
+    notify(options)
+  end
+
+  define_debt_email = ->(name, admin_method) {
+    define_method name do |debt|
+      @debt = debt
+      @administrator = debt.send(admin_method)
+      notify(mail_header_params(debt, name))
+    end
+  }
+
+  define_debt_email["debt_created", :author]
+  define_debt_email["debt_paid", :last_updated_by]
+  define_debt_email["debt_cancelled", :last_updated_by]
+
+  private
+
+
+  def notify(options)
+    mail(options) { |format| format.text }
+  end
+
+  def mail_header_params(source, subject)
+    {
+      to: source.person.email,
+      subject: I18n.t("mailers.notifier.#{subject}"),
+      cc: source.last_updated_by.email,
+      reply_to: source.last_updated_by.email
     }
-    options.merge!(cc: purchase.business_unit.email) if purchase.business_unit.try(:email)
-    mail(options) do |format|
-      format.text
-    end
-  end
-
-  def debt_created(debt)
-    @debt = debt
-    @administrator = debt.author
-    mail(to: @debt.person.email,
-         subject: I18n.t('mailers.notifier.debt_created'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
-    end
-  end
-
-  def debt_paid(debt)
-    @debt = debt
-    @administrator = debt.last_updated_by
-    mail(to: @debt.person.email,
-         subject: I18n.t('mailers.notifier.debt_paid'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
-    end
-  end
-
-  def debt_cancelled(debt)
-    @debt = debt
-    @administrator = debt.last_updated_by
-    mail(to: @debt.person.email,
-         subject: I18n.t('mailers.notifier.debt_paid'),
-         cc: @administrator.email,
-         reply_to: @administrator.email
-        ) do |format|
-      format.text
-    end
   end
 end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,5 +1,209 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Notifier do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "purchases" do
+    before(:all) do
+      @admin = Factory :person, role: 'admin'
+      PaperTrail.enabled = true
+      PaperTrail.whodunnit = @admin.id.to_s
+      @purchase = Factory :purchase
+    end
+
+    before(:each) { ActionMailer::Base.deliveries = [] }
+
+    describe "purchase_created" do
+      subject { Notifier.purchase_created(@purchase) }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @purchase.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.purchase_created')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har nu registrerat ditt inköp ##{@purchase.id}."
+        )
+      end
+    end
+
+    describe "purchase_approved" do
+      subject { Notifier.purchase_approved(@purchase) }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @purchase.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.purchase_approved')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har nu registrerat ditt inköp ##{@purchase.id} som godkänt."
+        )
+      end
+    end
+
+    describe "purchase_denied" do
+      subject { Notifier.purchase_denied(@purchase) }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @purchase.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.purchase_denied')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har nu registrerat ditt inköp ##{ @purchase.id } som avslaget."
+        )
+      end
+    end
+
+    describe "purchase_paid" do
+      subject { Notifier.purchase_paid(@purchase) }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har nu registrerat ditt inköp ##{@purchase.id} som betalt, du borde få pengarna inom några dagar."
+        )
+      end
+
+      context "business_unit with email" do
+        before(:all) { @purchase.business_unit.update_column(:email, "a@a.a") }
+
+        it "sets the correct to, from, and reply addresses" do
+          subject.to.should       == [ @purchase.person.email ]
+          subject.subject.should  == I18n.t('mailers.notifier.purchase_paid')
+          subject.cc.should       == [ @purchase.business_unit.email ]
+          subject.reply_to.should == [ @admin.email ]
+        end
+      end
+
+      context "business_unit with no email" do
+        before(:all) { @purchase.business_unit.update_column(:email, nil) }
+
+        it "sets the correct to, from, and reply addresses" do
+          subject.to.should       == [ @purchase.person.email ]
+          subject.subject.should  == I18n.t('mailers.notifier.purchase_paid')
+          subject.cc.should       == [ @admin.email ]
+          subject.reply_to.should == [ @admin.email ]
+        end
+      end
+    end
+  end
+
+  describe "debts" do
+
+    before(:all) do
+      @admin = Factory :person, role: 'admin'
+      PaperTrail.enabled = true
+      PaperTrail.whodunnit = @admin.id.to_s
+      @debt = Factory :debt
+    end
+
+    describe "debt_created" do
+      subject { Notifier.debt_created(@debt) }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @debt.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.debt_created')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har registrerat att du har en skuld till Konglig Datasektionen enligt nedan:"
+        )
+      end
+    end
+
+    describe "debt_paid" do
+      subject { Notifier.debt_paid(@debt).deliver }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @debt.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.debt_paid')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har registrerat att din skuld till Konglig Datasektionen markerats som betald."
+        )
+      end
+    end
+
+    describe "debt_cancelled" do
+      subject { Notifier.debt_cancelled(@debt).deliver }
+
+      before(:each) { subject.deliver }
+
+      it "delivers the email" do
+        subject.deliver
+        ActionMailer::Base.deliveries.should_not be_empty
+      end
+
+      it "sets the correct to, from, and reply addresses" do
+        subject.to.should       == [ @debt.person.email ]
+        subject.subject.should  == I18n.t('mailers.notifier.debt_cancelled')
+        subject.cc.should       == [ @admin.email ]
+        subject.reply_to.should == [ @admin.email ]
+      end
+
+      it "renders the body correctly" do
+        assert subject.body.to_s.starts_with?(
+          "#{ @admin.name } har strukit din skuld ##{ @debt.id } till datasektionen."
+        )
+      end
+    end
+  end
 end
