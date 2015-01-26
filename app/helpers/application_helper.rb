7d5a2c18e71e3380c60ef70fa88008903d54ce73
Martin Frost <frost@ceri.se>
2015-01-26 16:56:50 +0100

    removed some unused code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,4 @@
 module ApplicationHelper
-  def logged_in?
-    session[:cas_user]
-  end
-
-  def session_change_link
-    if user_signed_in?
-      link_to 'Logga ut', destroy_user_session_path, class: 'session-change'
-    else
-      link_to 'Logga in', new_session_path, class: 'session-change'
-    end
-  end
-
   def status(model)
     I18n.t model.workflow_state, scope: [:workflow_state]
   end
