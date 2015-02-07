67eb779723361ba1168ed1e7a381eacd1cb6cf93
Martin Frost <frost@ceri.se>
2015-02-07 09:56:13 +0100

    Fix what transpec failed to upgrade
    Some controllers try to DRY up their mocking in a way that transpec apparently
    didn't manage to fix.

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe PeopleController do
   def mock_person(stubs = {})
+    stubs = stubs.reverse_merge(to_str: nil)
     @mock_person ||= mock_model(Person, stubs).as_null_object
   end
 
   describe 'unauthenticated user' do
     it 'should be redirected to the login page' do
       get :index
-      response.should redirect_to(root_url)
+      expect(response).to redirect_to(root_url)
     end
   end
 
@@ -26,32 +27,32 @@ describe PeopleController do
     describe 'GET index' do
       it 'assigns all people as @people' do
         get :index
-        assigns(:people).should eq(Person.all)
+        expect(assigns(:people)).to eq(Person.all)
       end
     end
 
     describe 'GET show' do
       # render_views
       it 'assigns the requested person as @person' do
-        Person.stub(:find).with('37') { mock_person }
+        allow(Person).to receive(:find).with('37') { mock_person }
         get :show, id: '37'
-        assigns(:person).should be(mock_person)
+        expect(assigns(:person)).to be(mock_person)
       end
     end
 
     describe 'GET new' do
       it 'assigns a new person as @person' do
-        Person.stub(:new) { mock_person }
+        allow(Person).to receive(:new) { mock_person }
         get :new
-        assigns(:person).should be(mock_person)
+        expect(assigns(:person)).to be(mock_person)
       end
     end
 
     describe 'GET edit' do
       it 'assigns the requested person as @person' do
-        Person.stub(:find).with('37') { mock_person }
+        allow(Person).to receive(:find).with('37') { mock_person }
         get :edit, id: '37'
-        assigns(:person).should be(mock_person)
+        expect(assigns(:person)).to be(mock_person)
       end
     end
 
@@ -60,38 +61,38 @@ describe PeopleController do
         @ability = Object.new
         @ability.extend(CanCan::Ability)
         @ability.can :manage, Person
-        @controller.stub(:current_ability).and_return(@ability)
+        allow(@controller).to receive(:current_ability).and_return(@ability)
       end
 
       describe 'with valid params' do
         it 'assigns a newly created person as @person' do
-          Person.stub(:new).with('these' => 'params') { mock_person(save: true) }
+          allow(Person).to receive(:new).with('these' => 'params') { mock_person(save: true) }
           post :create, person: { 'these' => 'params' }
-          assigns(:person).should be(mock_person)
+          expect(assigns(:person)).to be(mock_person)
         end
 
         # TODO: PeopleController#create does some magic with ldap, since we can't have arbitrary users...
         # the controller tries to go to /people/some-user-name
         it 'redirects to the created person' do
           @ability.can(:manage, :all)
-          @controller.stub(:current_ability).and_return(@ability)
-          Person.stub(:new) { mock_person(save: true) }
+          allow(@controller).to receive(:current_ability).and_return(@ability)
+          allow(Person).to receive(:new) { mock_person(save: true) }
           post :create, person: {}
-          response.should redirect_to(person_url(mock_person))
+          expect(response).to redirect_to(person_url(mock_person))
         end
       end
 
       describe 'with invalid params' do
         it 'assigns a newly created but unsaved person as @person' do
-          Person.stub(:new).with('these' => 'params') { mock_person(save: false) }
+          allow(Person).to receive(:new).with('these' => 'params') { mock_person(save: false) }
           post :create, person: { 'these' => 'params' }
-          assigns(:person).should be(mock_person)
+          expect(assigns(:person)).to be(mock_person)
         end
 
         it "re-renders the 'new' template" do
-          Person.stub(:new) { mock_person(save: false) }
+          allow(Person).to receive(:new) { mock_person(save: false) }
           post :create, person: {}
-          response.should render_template('new')
+          expect(response).to render_template('new')
         end
       end
     end
@@ -99,35 +100,35 @@ describe PeopleController do
     describe 'PUT update' do
       describe 'with valid params' do
         it 'updates the requested person' do
-          Person.should_receive(:find).with('37') { mock_person }
-          mock_person.should_receive(:update_attributes).with('these' => 'params')
+          expect(Person).to receive(:find).with('37') { mock_person }
+          expect(mock_person).to receive(:update_attributes).with('these' => 'params')
           put :update, id: '37', person: { 'these' => 'params' }
         end
 
         it 'assigns the requested person as @person' do
-          Person.stub(:find) { mock_person(update_attributes: true) }
+          allow(Person).to receive(:find) { mock_person(update_attributes: true) }
           put :update, id: '1'
-          assigns(:person).should be(mock_person)
+          expect(assigns(:person)).to be(mock_person)
         end
 
         it 'redirects to the person' do
-          Person.stub(:find) { mock_person(update_attributes: true) }
+          allow(Person).to receive(:find) { mock_person(update_attributes: true) }
           put :update, id: '1'
-          response.should redirect_to(person_url(mock_person))
+          expect(response).to redirect_to(person_url(mock_person))
         end
       end
 
       describe 'with invalid params' do
         it 'assigns the person as @person' do
-          Person.stub(:find) { mock_person(update_attributes: false) }
+          allow(Person).to receive(:find) { mock_person(update_attributes: false) }
           put :update, id: '1'
-          assigns(:person).should be(mock_person)
+          expect(assigns(:person)).to be(mock_person)
         end
 
         it "re-renders the 'edit' template" do
-          Person.stub(:find) { mock_person(update_attributes: false) }
+          allow(Person).to receive(:find) { mock_person(update_attributes: false) }
           put :update, id: '1'
-          response.should render_template('edit')
+          expect(response).to render_template('edit')
         end
       end
     end
