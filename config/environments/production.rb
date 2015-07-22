388f4ea2e00f6737a04a5960295a5309c67db084
Martin Frost <frost@ceri.se>
2015-07-22 20:04:55 +0200

    Set proper MAGE config in configuration.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Cashflow::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_files = true
-  
+
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
